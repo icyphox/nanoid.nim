9ae539fa6cb0ac20fa466151e23bbbd3ceaf3a1d
Anirudh <icyph0x@pm.me>
2018-10-01 19:08:16 +0530

    urandom() now returns a `seq[char]`
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- nanoidpkg/urandom.nim
+++ nanoidpkg/urandom.nim
@@ -1,7 +1,10 @@
-proc urandom*(): int =
+import 
+  sequtils,
+  strutils
+
+proc urandom*(n: int): seq[char] =
   var f = open("/dev/urandom")
   var r: int
-  
-  discard f.readBuffer(addr r, 4)
+  discard f.readBuffer(addr r, n)
   close(f)
-  result = r
+  result = @($r)

634ee7aea05c8ffa99a19e72af41e2acead9281e
Anirudh <icyph0x@pm.me>
2018-10-06 15:19:05 +0530

    Test for collision in 10k entries
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,4 +1,6 @@
-import nanoid, unittest
+import
+  unittest,
+  ../src/nanoid
 
 suite "nanoid Test Suite":
   test "has correct size":
@@ -6,3 +8,10 @@ suite "nanoid Test Suite":
     for i in countUp(0, count):
       check(len(generate()) == 21)
 
+  test "has no collisions for 10k entries as 21 chars long":
+    let count = 100_000
+    var generated: seq[string] = @[]
+    for i in countUp(0, count):
+      var id = generate()
+      generated.add(id)
+    check(generate() in generated == false)

d2e3830e4608a5466984d38924fd5af26046b6fb
Anirudh <icyph0x@pm.me>
2018-11-07 14:03:42 +0530

    Add nanoidpkg/nonsecure (#2)
    This implements a non-secure predictable random generator
    
    Signed-off-by: Anirudh <icyph0x@pm.me>

---
+++ src/nanoidpkg/nonsecure.nim
@@ -0,0 +1,13 @@
+import 
+  random,
+  lenientops,
+  math
+
+let a = "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var s = 21
+
+proc generateNonSecure*(alphabet: string = a, size: int = s): string =
+  result = ""
+  randomize()
+  for i in countup(0, size):
+    result &= alphabet[(rand(1.0) * len(alphabet)).int]
